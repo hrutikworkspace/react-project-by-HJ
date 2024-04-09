1409400e28b6f39234ff0c2fe543d5c378c716bc
hrutikworkspace <hrutikjaiswal149@gmail.com>
2024-04-09 22:47:43 +0530

    changes in App

--- App.js
+++ App.js
@@ -1,6 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("h1", {id: "heading"}, "Hello World from React JS");
+// const heading = React.createElement("h1", {id:"heading"}, "Epsiode3");
+ 
+// What is JSX ?
+// sol:- a. JSX is just like a HTML but it is not a HTML 
+
+const headerTag = <h1 id="heading">Epsiode 3...</h1>
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+
+root.render(headerTag)
